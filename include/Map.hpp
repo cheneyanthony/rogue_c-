4bc4f55b5cc279cc565c657d13afd15720150847
cheneyanthony <cheneyanthony@gmail.com>
2019-01-02 11:17:03 -0800

    Added more code

--- include/Map.hpp
+++ include/Map.hpp
@@ -13,6 +13,16 @@ public :
  	void render() const;
 protected :
     Tile *tiles;
- 
-    void setWall(int x, int y);
+
+    friend class BspListener;
+
+    void dig(int x1, int y1, int x2, int y2);
+    void createRoom(bool first, int x1, int y1, int x2, int y2);
 };
+
+class BspListener : public ITCODBspCallback {
+    private:
+        Map &map; // a map to dig
+        int roomNum; //room order
+        int lastx,lasty; // center of the last room
+}
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,10 +1,15 @@
 #include "libtcod.hpp"
 #include "Map.hpp"
 
+static const int ROOM_MAX_SIZE = 12;
+static const int ROOM_MIN_SIZE = 6;
+
 Map::Map(int width, int height) : width(width),height(height) {
     tiles=new Tile[width*height];
-    setWall(30,22);
-    setWall(50,22);
+    TCODBsp bsp(0,0,width,height);
+    bsp.splitRecursive(NULL,8,ROOM_MAX_SIZE,ROOM_MAX_SIZE,1.5f,1.5f);
+    BspListener listener(*this);
+    bsp.traverseInvertedLevelOrder(&listener, NULL);
 }
 
 Map::~Map() {
@@ -15,8 +20,37 @@ bool Map::isWall(int x, int y) const {
     return !tiles[x+y*width].canWalk;
 }
  
-void Map::setWall(int x, int y) {
-    tiles[x+y*width].canWalk=false;
+void Map::dig(int x1, int y1, int x2, int y2) {
+    if( x2 < x1){
+        int tmp = x2;
+        x2 = x1;
+        x1 = tmp;
+    }
+    if (y2 < y1) {
+        int tmp = y2;
+        y2 = y1;
+        y1 = tmp;
+    }
+    for (int tilex=x1; tilex <= x2; tilex++){
+        for(int tiley=y1;tiley <= y2; tiley++){
+            tiles[tilex + tiley*width].canWalk=true;
+        }
+    }
+}
+
+void Map::createRoom(bool int x1, int y1, int x2, int y2){
+    dig(x1,y1,x2,y2);
+    if (first){
+        //put player in the first room
+        engine.player->x=(x1+x2)/2;
+        engine.player->y=(y1+y2)/2;
+    } else {
+        TCODRandom *rng=TCODRandom::getInstance();
+        if ( rng->getInt(0,3)==0) {
+            engine.actors.push(new Actor((x1+x2)/2,(y1+y2)/2,'@',
+                TCODColor::yellow))
+        }
+    }
 }
 
 void Map::render() const {
